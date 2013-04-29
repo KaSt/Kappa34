e5cdbfe8709fb70c05d8a05b21fd414b1cbf90f8
KaSt <k@ka.st>
2013-04-29 14:35:37 +0200

    Initial Commit of Kappa Kernel based on Sony's 3.4 (Xperia Z Sources)

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#0 PREEMPT Mon Apr 29 12:35:08 CEST 2013"
+#define LINUX_COMPILE_BY "ka"
+#define LINUX_COMPILE_HOST "ti.ka.st"
+#define LINUX_COMPILER "gcc version 4.7.3 20130102 (prerelease) (Linaro GCC 4.7-2013.01) "
