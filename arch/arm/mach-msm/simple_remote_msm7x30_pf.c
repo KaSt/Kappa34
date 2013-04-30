a4e483404258bfebe8ce5cb251fe1b398b22ee7a
KaSt <k@ka.st>
2013-04-30 11:42:46 +0200

    SimpleRemote 7x30 drivers (Thanks to @irii-soft)

---
+++ arch/arm/mach-msm/simple_remote_msm7x30_pf.c
@@ -0,0 +1,11 @@
+--2013-04-30 11:41:30--  https://raw.github.com/irii-soft/CAF_msm7x30-3.4.x/35739374f8b1ecbe04050c60d57c713c1dc9e9ca/arch/arm/mach-msm/simple_remote_msm7x30_pf.c
+Resolving raw.github.com... 199.27.76.133
+Connecting to raw.github.com|199.27.76.133|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 45512 (44K) [text/plain]
+Saving to: “simple_remote_msm7x30_pf.c”
+
+     0K .......... .......... .......... .......... ....      100% 25.6M=0.002s
+
+2013-04-30 11:41:30 (25.6 MB/s) - “simple_remote_msm7x30_pf.c” saved [45512/45512]
+
